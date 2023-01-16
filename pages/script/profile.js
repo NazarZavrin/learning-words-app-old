fb04ad40e9bdd66df4a5c4ee3494fc5782ce751b
nazar_zavrin <nzavrin1404@gmail.com>
2023-01-16 15:10:50 +0200

    dev 2.2

--- pages/script/profile.js
+++ pages/script/profile.js
@@ -4,8 +4,8 @@ console.log("profile");
 const infoBtn = document.querySelector(".header__info-btn");
 const profileLabel = document.querySelector(".header__profile-label");
 const infoSidebar = document.querySelector(".profile-info");
-infoBtn.classList.remove("active");// when page loads
-infoSidebar.classList.remove("active");// when page loads
+infoBtn.classList.add("active");// when page loads remove
+infoSidebar.classList.add("active");// when page loads remove
 infoBtn.addEventListener("click", event => {
     infoBtn.classList.toggle("active");
     infoSidebar.classList.toggle("active");
@@ -14,3 +14,12 @@ profileLabel.addEventListener("click", event => {
     event.preventDefault();
     infoBtn.click();
 })
+infoSidebar.addEventListener("click", event => {
+    if (event.target.closest(".edit-btn")) {
+        // event.target.closest(".edit-btn").previousElementSibling.textContent = "";
+        console.log(HTMLElement.prototype);
+        event.target.closest(".edit-btn").parentElement.nextElementSibling.style.display = "flex";
+    }
+})
+
+
